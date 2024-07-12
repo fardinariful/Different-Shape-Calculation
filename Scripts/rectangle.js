c8fc326e6111a9e2d68c9e5f6d687d1724e22af5
fardinariful <fardinewucse2020@gmail.com>
2024-07-13 00:28:08 +0600

    new file added

---
+++ Scripts/rectangle.js
@@ -0,0 +1,17 @@
+function calculatedRectangle(){
+    const Rectanglelength= document.getElementById('rectangle_length');
+    const rectanglelengthtext= Rectanglelength.value;
+    const length= parseFloat(rectanglelengthtext);
+    console.log(length);
+
+    const RectangleWidth=document.getElementById('rectangle_width');
+    const rectanglewidthtext=RectangleWidth.value;
+    const width=parseFloat(rectanglewidthtext);
+    console.log(width);
+    
+    const area = length*width;
+    
+    const rectanglearea =document.getElementById('rectangle_area');
+    rectanglearea.innerText=area;
+
+}
--- Scripts/traingle.js
+++ Scripts/traingle.js
@@ -16,3 +16,5 @@ function calculatedTraingle(){
     traingleareaspan.innerText = area;
 
 }
+
+
