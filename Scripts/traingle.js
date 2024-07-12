bd2e5b07162979ef52847d5def68d4392245aec3
fardinariful <fardinewucse2020@gmail.com>
2024-07-12 23:58:51 +0600

    value added

--- Scripts/traingle.js
+++ Scripts/traingle.js
@@ -1,3 +1,14 @@
 function calculatedTraingle(){
-    console.log('inside function');
+    const trainglebase=document.getElementById('traingle-base');
+    const traingletext=trainglebase.value;
+    const base=parseFloat(traingletext);
+    console.log(base);
+
+    const traingleheight= document.getElementById('traingle-height');
+    const traingletext2=traingleheight.value;
+    const height= parseFloat(traingletext2);
+    console.log(height);
+
+
+
 }
